d87766e1ee501c20f3ff114c99d762db0e287954
David Mebo <mebodave@gmail.com>
2024-10-26 13:42:37 +0100

    cleanup #2

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,17 +2,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck-rust projectile highlight-indent-guides format-all org-modern flycheck-pos-tip flycheck-color-mode-line flycheck markdown-mode treesit-auto cape corfu embark-consult embark marginalia orderless vertico doom-modeline kind-icon nerd-icons-dired nerd-icons-completion dashboard doom-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'dired-find-alternate-file 'disabled nil)
