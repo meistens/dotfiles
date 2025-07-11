a4d06e12a422129271f3273a9d83dbb6b420d94e
David Mebo <krankd@proton.me>
2025-07-11 01:56:31 +0100

    rest of code with Redis

--- golang+monitoring/internal/handlers/user.go
+++ golang+monitoring/internal/handlers/user.go
@@ -3,12 +3,16 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"skello/internal/cache"
 	"skello/internal/db"
 	"skello/internal/logger"
 	"skello/internal/metrics"
 	"skello/internal/models"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 // UserHandler handles user-related requests
@@ -42,5 +46,27 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Database error", http.StatusInternalServerError)
 		return
 	}
+	metrics.DBOps.WithLabelValues("insert", "success").Inc()
 
+	// cache, this scenario is the users
+	userJSON, _ := json.Marshal(user)
+	cacheKey := fmt.Sprintf("user:%d", user.ID)
+	err = cache.Get().Set(ctx, cacheKey, userJSON, time.Hour).Err()
+
+	if err != nil {
+		logger.Get().WithError(err).Error("Failed to cache user in Redis")
+		metrics.RedisOps.WithLabelValues("set", "error").Inc()
+	} else {
+		metrics.RedisOps.WithLabelValues("set", "success")
+	}
+
+	logger.Get().WithFields(logrus.Fields{
+		"user_id": user.ID,
+		"name":    user.Name,
+		"email":   user.Email,
+	}).Info("User created successfully")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
 }
