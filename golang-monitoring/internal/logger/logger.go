7fa5ac4df9439df02d72bdf52d2c083144711b74
David Mebo <krankd@proton.me>
2025-07-12 17:11:57 +0100

    log to file and stdout
    part of the main app not showing on Grafana

--- golang-monitoring/internal/logger/logger.go
+++ golang-monitoring/internal/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"io"
 	"log"
 	"os"
 
@@ -22,8 +23,13 @@ func Init() {
 	file, err := os.OpenFile("/app/logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Printf("Failed to open log file: %v", err)
+		// stdout if file fails, not in the mood to relive debugging a tool that lacks
+		// a clear documentaton and relies on blogs and mostly the community to do
+		// the docs
+		logger.SetOutput(os.Stdout)
 	} else {
-		logger.SetOutput(file)
+		multiWriter := io.MultiWriter(os.Stderr, file)
+		logger.SetOutput(multiWriter)
 	}
 }
 
